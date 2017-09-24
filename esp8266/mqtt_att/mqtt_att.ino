507558dd224240ca7741d7b052969f85468a0564
Zoran <sprintrz@gmail.com>
2017-09-24 18:39:59 +0200

    Add files via upload

--- esp8266/mqtt_att/mqtt_att.ino
+++ esp8266/mqtt_att/mqtt_att.ino
@@ -16,17 +16,13 @@ char deviceId[] = "sxBP79eJDwOelE3TFIufXTLY";
 char token[] = "maker:4PoWRlpZZautW1VeVvQHWBD5LcibxnmtOnAqzT6";
 
 char outTopicCounter[] = "device/sxBP79eJDwOelE3TFIufXTLY/asset/counter/state";
-char outTopicText[] = "device/sxBP79eJDwOelE3TFIufXTLY/asset/poruka/state";
 char inTopic[] = "device/sxBP79eJDwOelE3TFIufXTLY/asset/+/command";
-
-String value1 = "\"value\":\"1\"";
-String value0 = "\"value\":\"0\"";
 
 WiFiClient espClient;
 PubSubClient client(espClient);
 long lastMsg = 0;
 char msg[50];
-int value = 0;
+
 long int counter = 0;
 
 void setup() {
@@ -68,21 +64,16 @@ void callback(char* topic, byte* payload, unsigned int length) {
   Serial.print("] ");
   for (int i = 0; i < length; i++) {
     json[i] = (char)payload[i];
-    //Serial.print((char)payload[i]);
   }
   json[length] = '\0';
-  //Serial.println(json);
+  
   JsonObject& root = jsonBuffer.parseObject(json);
 
   if (root.success()) {
-    const char* atTime = root["at"];
     const char* vrednost = root["value"];
-    Serial.println(atTime);
     Serial.println(vrednost);
     if (strcmp(vrednost,"1") == 0) {
       digitalWrite(BUILTIN_LED, LOW);   // Turn the LED on (Note that LOW is the voltage level
-      // but actually the LED is on; this is because
-      // it is acive low on the ESP-01)
     } else {
       digitalWrite(BUILTIN_LED, HIGH);  // Turn the LED off by making the voltage HIGH
     }
@@ -123,38 +114,7 @@ void loop() {
     ++counter;
     String value;
     value = String(counter);
-    /*
-      snprintf (msg, 75, "0|Counter : %s",value.c_str());
-      Serial.print("Publish to poruka : ");
-      Serial.println(msg);
-      client.publish(outTopicText, msg);
-      delay(100);
-    */
     snprintf (msg, 75, "0|%s", value.c_str());
-    //Serial.print("Publish to counter: ");
-    //Serial.println(msg);
     client.publish(outTopicCounter, msg);
-
   }
-}
-
-
-char* buildContent(String value)
-{
-  char* message_buff;
-  int length;
-
-  if (value[0] == '[' || value[0] == '{') {
-    length = value.length() + 16;
-    message_buff = new char[length];
-    sprintf(message_buff, "{\"value\":%s}", value.c_str());
-  }
-  else {
-    length = value.length() + 3;
-    message_buff = new char[length];
-    sprintf(message_buff, "0|%s", value.c_str());
-  }
-
-  message_buff[length - 1] = 0;
-  return message_buff;
 }
